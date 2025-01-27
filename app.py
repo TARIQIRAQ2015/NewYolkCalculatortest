d24f78903e0729463dd9d96d52f95e66afec88cc
Tariq Al-Yassin <106028337+TARIQIRAQ2015@users.noreply.github.com>
2025-01-27 18:01:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -278,7 +278,7 @@ if calculation_type == texts[language]["chicken_profits"]:
                     "Category": [
                         texts[language]["new_egg_price"],
                         texts[language]["new_feed_price"],
-                        "Rent payment"
+                        texts[language]["Rent payment"]
                     ],
                     "Value": [
                         total_egg_price,
