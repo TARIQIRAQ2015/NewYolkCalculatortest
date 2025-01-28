d0451eee28ec2c2e9b6f4fdd2f31b7c9ce205d4c
Tariq Al-Yassin <106028337+TARIQIRAQ2015@users.noreply.github.com>
2025-01-28 06:29:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -497,18 +497,17 @@ if st.button(texts[language]["reset"], type="secondary"):
 st.markdown(
     """
     <div style="text-align: center; margin-top: 30px;">
-        <a href="https://www.facebook.com/newyolkfarming" target="_blank" style="text-decoration: none; margin: 0 10px;">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg" width="32" height="32" alt="Facebook">
-        </a>
-        <a href="https://t.me/newyolkfarm" target="_blank" style="text-decoration: none; margin: 0 10px;">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/8/82/Telegram_logo.svg" width="32" height="32" alt="Telegram">
+        <a href="https://farm.newyolk.io/" target="_blank" style="text-decoration: none; margin: 0 10px;">
+            <img src="https://i.ibb.co/YDKWBRf/internet.png" width="32" height="32" alt="Website">
         </a>
         <a href="https://discord.gg/RYDExGGWXh" target="_blank" style="text-decoration: none; margin: 0 10px;">
             <img src="https://assets-global.website-files.com/6257adef93867e50d84d30e2/636e0a6a49cf127bf92de1e2_icon_clyde_blurple_RGB.png" width="32" height="32" alt="Discord">
         </a>
-        <a href="https://farm.newyolk.io/" target="_blank" style="text-decoration: none; margin: 0 10px;">
-            <span style="color: #007bff; font-weight: bold; font-size: 16px;">Website</span>
-            <img src="https://i.ibb.co/YDKWBRf/internet.png" width="32" height="32" alt="Website" style="margin-left: 5px;">
+        <a href="https://t.me/newyolkfarm" target="_blank" style="text-decoration: none; margin: 0 10px;">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/8/82/Telegram_logo.svg" width="32" height="32" alt="Telegram">
+        </a>
+        <a href="https://www.facebook.com/newyolkfarming" target="_blank" style="text-decoration: none; margin: 0 10px;">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg" width="32" height="32" alt="Facebook">
         </a>
         <br>
         <br>
@@ -521,9 +520,6 @@ st.markdown(
         a img:hover {
             transform: scale(1.2);
             filter: brightness(1.2);
-        }
-        a:hover span {
-            text-decoration: underline;
         }
     </style>
     """,
