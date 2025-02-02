d44909d4c5ce439f103464fbd831ad43d909d9bc
Tariq Al-Yassin <106028337+TARIQIRAQ2015@users.noreply.github.com>
2025-02-02 10:09:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1190,12 +1190,9 @@ st.markdown("""
             }
         });
 
-        // التمرير السلس إلى الأعلى
+        // التمرير السلس إلى الرابط المحدد
         function scrollToTop() {
-            window.scrollTo({
-                top: 0,
-                behavior: 'smooth'
-            });
+            window.location.href = 'https://testnewyolkcalculatortest.streamlit.app/~/+/#:~:text=%D8%AD%D8%B3%D8%A7%D8%A8%20%D8%A3%D8%B1%D8%A8%D8%A7%D8%AD%20%D8%A7%D9%84%D8%AF%D8%AC%D8%A7%D8%AC%20%D9%88%D8%A7%D9%84%D9%85%D9%83%D8%A7%D9%81%D8%A2%D8%AA%20%D8%A7%D9%84%D9%8A%D9%88%D9%85%D9%8A%D8%A9';
         }
     </script>
 
